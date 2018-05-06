eaa53abc4146782b33b73da512a75d7f30d2d643
Daniel Hartnell <danielhartnell@gmail.com>
2018-05-06 14:30:52 -0700

    detached from docker compose up

--- vulnscanning/start-clair.sh
+++ vulnscanning/start-clair.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-docker-compose up -d postgres
-sleep 3
-docker-compose up
+docker-compose up -d
 curl -sL https://github.com/arminc/clair-scanner/releases/download/v8/clair-scanner_linux_amd64 -o clair-scanner
 chmod +x clair-scanner
+sleep 10
 clair-scanner --clair="http://127.0.0.1:6060" --ip $(hostname -i) redirector
 
