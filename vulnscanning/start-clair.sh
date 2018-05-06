6cfab0884ee52bc4cd875cb98d32515859889a22
Daniel Hartnell <danielhartnell@gmail.com>
2018-05-06 14:34:03 -0700

    provided path to executable

--- vulnscanning/start-clair.sh
+++ vulnscanning/start-clair.sh
@@ -4,5 +4,5 @@ docker-compose up -d
 curl -sL https://github.com/arminc/clair-scanner/releases/download/v8/clair-scanner_linux_amd64 -o clair-scanner
 chmod +x clair-scanner
 sleep 10
-clair-scanner --clair="http://127.0.0.1:6060" --ip $(hostname -i) redirector
+./clair-scanner --clair="http://127.0.0.1:6060" --ip $(hostname -i) redirector
 
